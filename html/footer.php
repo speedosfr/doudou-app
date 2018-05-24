4ed6cdb829660493b79bff7537da3e182a29487f
speedosfr <dynaouest@gmail.com>
2018-05-24 11:02:34 +0200

    envoi complet

---
+++ html/footer.php
@@ -0,0 +1,6 @@
+</div>
+   <main id="content"></main>
+    <script src="js/jquery.js"></script>  
+</body>
+</html>
+<footer><?php echo date("Y"); ?></footer>
--- js/app.js
+++ js/app.js
@@ -1,48 +1,12 @@
-randomImg();
+$("#search_btn").click(function(){
+    $("main").css("display","none");
+    $('#form_search').css("display","block");
+    console.log("je passe devant le bouton");
+});
+$("#send_btn").click(function(){
+    $("main").css("display","block");
+    $('#form_search').css("display","none");
+    console.log("je passe devant le envoyer");
+});
 
-function randomImg(){
-    
 
-}
-
-function serachAndDisplayDoudou() {
-
-    $.ajax({
-        url :"http://localhost:8082/doudou/doudou-sf/public/api/v1/doudou",
-        method : "GET",
-        data: {
-            q: keyword
-        }
-    })
-    .done(function(response){
-        
-      //  window.navigator.vibrate(200);
-        //Enleve le loading
-        $("#content").empty();
-
-        response.data.forEach(function(movie){
-            var pic = $("<img>").attr("src", "http://localhost:8082/movies-sf/public/img/posters/" + movie.image)
-            pic.css("width", 200)
-            pic.attr("data-movieId", movie.id)
-
-            $("#content").append(pic)
-            
-        })
-
-        $("body").on("click","img", function(){
-            var id = $(this).attr("data-movieId")                
-            serachDetails(id)
-        });
-
-        function serachDetails(id) {
-            $.ajax({
-                url :"http://localhost:8082/movies-sf/public/api/v1/movies",
-                method : "GET",
-                data: {
-                    q: id
-                }
-            })
-            console.log(id)
-        }
-    })
-}
