1604f30355920a55538c74e2276d4a3b3a782b5d
speedosfr <dynaouest@gmail.com>
2018-05-25 12:28:13 +0200

    Commit avant la fin du projet

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 show_all();
 
 $("#search_btn").click(function () {
-    $("main").css("display", "none");
-    $('#form_search').css("display", "block");
-    console.log("je passe devant le bouton");
+    $("main").toggle();
+    $('#form_search').toggle();
+   
 });
 $("#send_btn").click(function () {
-    $("main").css("display", "block");
-    $('#form_search').css("display", "none");
-    console.log("je passe devant le envoyer");
+    $("main").toggle();
+    $('#form_search').toggle();;
+   
 });
 
 
@@ -37,9 +37,9 @@ function show_all() {
             $("#content").on("click", "img", function () {
                 var id = $(this).attr("data.id")
                
-                $("main").css("display", "none");
-                $("#titre").css("display", "none");
-                $('#details_doudou').css("display", "block");
+                $("main").toggle();
+                $("#titre").toggle();
+                $('#details_doudou').toggle();
                 searchDetails(id,response.data)
 
                 console.log("test" +id)
@@ -53,27 +53,35 @@ function show_all() {
 
                 if (id == data[i].id) { 
                     
-                    $("#details_doudou").empty();
-                    var pic = $("<img>").attr("src", "http://localhost:8082/Doudou/doudou-sf/public/img/photos/" + data[id].image)
+                    console.log(data);
+                    console.log("id " +data[i].id);
+                    
+                    $("#pic_doudou").empty();
+                    var pic = $("<img>").attr("src", "http://localhost:8082/Doudou/doudou-sf/public/img/photos/" + data[i].image)
                     pic.css("width", 200)
                     pic.css("height", 300)
                     pic.css("border", "2px solid white")
                     pic.css("border-radius", "25px")
         
-                    var color = data[id].color
-                    var date = data[id].dateFind
-                    var place = data[id].placeFind
-                    var type = data[id].type
-                    var lat = data[id].lat
-                    var lng = data[id].lng
-        
-                    $("#details_doudou").html("<p>Ce doudou de couleur " + color + " a été trouvé le " + date + " à " + place + " qui est de type " + type +
-                        "</p>");
-                    $("#details_doudou").append(pic);          
+                    var color = data[i].color
+                    var date = data[i].dateFind
+                    var place = data[i].placeFind
+                    var type = data[i].type
+                    var lat = data[i].lat
+                    var lng = data[i].lng        
                     
-                }else{
-                    
-                }            
+                    $("#pic_doudou").append(pic);
+                    $("#details_doudou").append(
+
+                        `<div id ="show_details">
+                             <p> Couleur : ${color} </p>
+                             <p> Lieu de decouverte :${place} </p>
+                             <p></p>   Type du doudou : ${type} </p>                                
+                         </div>
+                        
+                        
+                        </p>`);              
+                }     
 
         }
 }
